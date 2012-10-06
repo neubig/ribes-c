da0281be3d2e442454543d63ab78d2176d40e019
Graham Neubig <neubig@gmail.com>
2012-10-06 11:53:15 +0900

    Added the license and README

--- ribes-c.cpp
+++ ribes-c.cpp
@@ -1,3 +1,34 @@
+//
+// ribes-c
+//  Copyright (C) 2012  Graham Neubig
+// (original Python code is)
+//  Copyright (C) 2011  Nippon Telegraph and Telephone Corporation
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// This program calculates the "RIBES" machine translation evaluation measure,
+// which is particularly useful for translation between language pairs with
+// large amounts of reordering.
+// 
+// Reference:
+//  Hideki Isozaki, Tsutomu Hirao, Kevin Duh, Katsuhito Sudoh, and Hajime Tsukada,
+//  "Automatic Evaluation of Translation Quality for Distant Language Pairs",
+//  Proceedings of the 2010 Conference on Empirical Methods in Natural Language Processing (EMNLP),
+//  pp. 944--952 Cambridge MA, October, 2010
+//  -- http://aclweb.org/anthology-new/D/D10/D10-1092.pdf
+
 #include <boost/unordered_map.hpp>
 #include <boost/program_options.hpp>
 #include <boost/format.hpp>
